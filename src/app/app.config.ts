20a01f8bc2c65e696ab5a58e93b0463d070a93fc
Guantai Jp <jpguantai@gmail.com>
2025-01-31 07:05:47 +0300

    converted to PWA

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,17 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
-
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { routes } from './app.routes';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideHttpClient(),
-      provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+    providers: [
+        provideHttpClient(withInterceptorsFromDi()),
+        provideZoneChangeDetection({ eventCoalescing: true }),
+        provideRouter(routes),
+        provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+        })
+    ]
 };
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,15 +7,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
-import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
     declarations: [
-
+        // AppComponent should be in declarations
     ],
     imports: [
         BrowserModule,
-        HttpClientModule,
+        HttpClientModule,    // Only import once
         AppRoutingModule,
         CoreModule,
         SharedModule,
