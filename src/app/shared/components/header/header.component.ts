6f432be9a76d77fa85270c2ad8096acc33614635
Guantai Jp <jpguantai@gmail.com>
2025-01-31 08:27:58 +0300

    nav in the header

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,10 +1,14 @@
 // src/app/shared/components/header/header.component.ts
 import { Component } from '@angular/core';
+import {RouterLink} from "@angular/router";
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
     standalone: true,
+    imports: [
+        RouterLink
+    ],
     styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
